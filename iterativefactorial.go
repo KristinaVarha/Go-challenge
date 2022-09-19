c5a923de117f523f6ea914d5b814a3c12ae68271
student <you@example.com>
2022-09-19 11:49:49 +0300

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,15 +1,18 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-   if nb < 0 {
-      return 0
-   }
-   fact := 1
-   for i := 1; i <= nb; i++ {
-      fact *= i
-	   if fact < 0 {
-	      return 0
-	   }
-   }
-   return fact
+	if nb < 25 {
+		if nb == 0 {
+			return 1
+		}
+		if nb < 0 {
+			return 0
+		}
+		var b = 1
+		for a := nb; a >= 1; a-- {
+			b *= a
+		}
+		return b
+	}
+	return 0
 }
