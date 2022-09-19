627447a2563e4eb5983e60c3606aadc0fff1a704
student <you@example.com>
2022-09-19 15:15:25 +0300

    iterativepower

--- iterativepower.go
+++ iterativepower.go
@@ -4,7 +4,7 @@ func IterativePower(nb int, power int) int {
 	var result int = 1
 	if power > 0 {
 		result = nb * (IterativePower(nb, power-1))
-	} else {
+	} else if power < 0 {
 		return 0
 	}
 	return result
