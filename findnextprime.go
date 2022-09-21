ac1023493215a9fb1a0d43075f889030e086d04f
student <you@example.com>
2022-09-21 07:32:03 +0300

    compare

--- findnextprime.go
+++ findnextprime.go
@@ -11,3 +11,20 @@ func FindNextPrime(nb int) int {
 		return n
 	}
 }
+
+func IsPrime2(nb int) bool {
+	if nb == 1 || nb <= 0 {
+		return false
+	}
+	prime := 1
+	for i := 2; i < nb; i++ {
+		if (nb % i) == 0 {
+			return false
+		}
+	}
+	if prime == 1 {
+		return true
+	} else {
+		return false
+	}
+}
