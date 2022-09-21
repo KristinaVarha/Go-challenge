87ed6b0e71163b8dabf2bb41070c7ed4c8ccde86
student <you@example.com>
2022-09-21 13:29:20 +0300

    programname

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -7,8 +7,15 @@ import (
 )
 
 func main() {
-	arg := os.Args
-	for _, char := range arg[0] {
+	lastSlashIndex := 0
+	programName := os.Args[0]
+	for index, char := range programName {
+		if char == '/' {
+			lastSlashIndex = index
+		}
+	}
+
+	for _, char := range programName[lastSlashIndex+1:] {
 		z01.PrintRune(char)
 	}
 	z01.PrintRune('\n')
