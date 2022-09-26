b17e496533dca42c11e273beb12bb136db576df9
student <you@example.com>
2022-09-26 20:13:52 +0300

    cat

--- cat/main.go
+++ cat/main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	size := len(os.Args)
-	for i := 1; i < size; i++ {
-		data, err := ioutil.ReadFile(os.Args[i])
+	lens := 0
+	argum := os.Args
+	for range argum {
+		lens++
+	}
+	for i := 1; i < lens; i++ {
+		file, err := ioutil.ReadFile(os.Args[i])
+		strFile := string(file)
 		if err != nil {
-			fmt.Println(err.Error())
+			errmessage := "open " + os.Args[i] + ": no such file or directory"
+			for _, v := range errmessage {
+				z01.PrintRune(v)
+			}
+			z01.PrintRune('\n')
+
+		} else {
+			for _, v := range strFile {
+				z01.PrintRune(v)
+			}
+		}
+	}
+	if lens == 1 {
+		_, err := io.Copy(os.Stdout, os.Stdin)
+		if err != nil {
+			str := err.Error()
+			for _, v := range str {
+				z01.PrintRune(v)
+			}
+			z01.PrintRune(10)
 			return
 		}
-		fmt.Println(string(data))
-		fmt.Println()
 	}
 }
