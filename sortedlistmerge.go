0a1a582fb362d0716f98ffdc9e24b528794ee10c
student <you@example.com>
2022-09-30 01:01:26 +0300

    sortedlistmerge

--- sortedlistmerge.go
+++ sortedlistmerge.go
@@ -1,8 +1,6 @@
 package piscine
 
 func SortedListMerge(n1 *NodeI, n2 *NodeI) *NodeI {
-	n1 = ListSort(n1)
-	n2 = ListSort(n2)
 	if n1 == nil {
 		return n2
 	}
@@ -12,8 +10,7 @@ func SortedListMerge(n1 *NodeI, n2 *NodeI) *NodeI {
 	if n1.Data <= n2.Data {
 		n1.Next = SortedListMerge(n1.Next, n2)
 		return n1
-	} else {
-		n2.Next = SortedListMerge(n1, n2.Next)
-		return n2
 	}
+	n2.Next = SortedListMerge(n1, n2.Next)
+	return n2
 }
